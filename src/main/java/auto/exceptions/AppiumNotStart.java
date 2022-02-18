3a8a68e0c4ae8201dc958df93351bda2ad905f88
jiyvn <1014054249@qq.com>
2022-02-18 23:55:36 +0800

    update appium exception

--- src/main/java/auto/exceptions/AppiumNotStart.java
+++ src/main/java/auto/exceptions/AppiumNotStart.java
@@ -1,10 +1,16 @@
 package auto.exceptions;
 
 public class AppiumNotStart extends JavAutoException {
+
     public AppiumNotStart(){
         super();
     }
+
     public AppiumNotStart(String message){
         super(message);
+    }
+
+    public AppiumNotStart(String message, Throwable cause){
+        super(message, cause);
     }
 }
