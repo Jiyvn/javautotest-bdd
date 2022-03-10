c3c37d8172db0c2bd46a270d50b797acbca00808
jiyvn <1014054249@qq.com>
2022-03-10 23:09:05 +0800

    set console level to info, file level to debug

--- src/main/java/auto/propertyLoader.java
+++ src/main/java/auto/propertyLoader.java
@@ -10,17 +10,18 @@ public class propertyLoader {
 
     public static void setDriverExecutor(){
         System.setProperty("webdriver.chrome.driver", home + "/webdrivers/chromedriver");
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+        // effective
+//        System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
 //        // ineffective
 //        System.setProperty(ChromeDriverService.CHROME_DRIVER_VERBOSE_LOG_PROPERTY, "false");
         System.setProperty(ChromeDriverService.CHROME_DRIVER_APPEND_LOG_PROPERTY, "false"); // append to the log file or not
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "chrome.log");
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "log/chrome.log");
         System.setProperty("webdriver.safari.driver", "/usr/bin/safaridriver");
         // firefox dev edition application
         System.setProperty("webdriver.firefox.bin", "/Applications/Firefox Developer Edition.app/Contents/MacOS/firefox");
         System.setProperty("webdriver.gecko.driver", home + "/webdrivers/geckodriver");
         System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE,"true");
-        System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"firefox.log");
+        System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"log/firefox.log");
 //        org.testng.log4testng.Logger
     }
 }
