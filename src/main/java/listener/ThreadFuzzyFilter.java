217481f07965f62a196443e2eb7fd4e61e047e0d
jiyvn <1014054249@qq.com>
2022-03-12 00:08:41 +0800

    add ThreadFuzzyFilter

---
+++ src/main/java/listener/ThreadFuzzyFilter.java
@@ -0,0 +1,33 @@
+package listener;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.filter.AbstractMatcherFilter;
+import ch.qos.logback.core.spi.FilterReply;
+
+public class ThreadFuzzyFilter extends AbstractMatcherFilter<ILoggingEvent> {
+    private String[] threadName;
+
+    public FilterReply decide(ILoggingEvent event) {
+        if (!this.isStarted()) {
+            return FilterReply.NEUTRAL;
+        } else {
+            for(String tn: this.threadName){
+                if(event.getThreadName().startsWith(tn)){
+                    return this.onMatch;
+                }
+            }
+            return this.onMismatch;
+        }
+    }
+
+    public void setThreadName(String threadName) {
+        this.threadName = threadName.split(",");
+    }
+
+    public void start() {
+        if (this.threadName != null) {
+            super.start();
+        }
+
+    }
+}
